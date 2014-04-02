25751004c2ffa07981cd263e03dbead77fa2a042
Ivan Sopov <sopov.ivan@gmail.com>
2014-04-02 22:46:39 +0400

    result on windows

--- src/main/java/com/sopovs/moradanen/jmh/SystemTimeBenchmark.java
+++ src/main/java/com/sopovs/moradanen/jmh/SystemTimeBenchmark.java
@@ -41,6 +41,12 @@ public class SystemTimeBenchmark {
 	//	c.s.m.j.SystemTimeBenchmark.testMiliTime               avgt        15       18.331        0.066    ns/op
 	//	c.s.m.j.SystemTimeBenchmark.testMillisFromNanoTime     avgt        15       16.837        0.134    ns/op
 	//	c.s.m.j.SystemTimeBenchmark.testNanoTime               avgt        15       16.167        0.228    ns/op
+
+    //  On Windows, Corei3 3120-M
+    //  Benchmark                                              Mode   Samples         Mean   Mean error    Units
+    //  c.s.m.j.SystemTimeBenchmark.testMiliTime               avgt        15       16.499        0.058    ns/op
+    //  c.s.m.j.SystemTimeBenchmark.testMillisFromNanoTime     avgt        15       19.472        0.311    ns/op
+    //  c.s.m.j.SystemTimeBenchmark.testNanoTime               avgt        15       18.108        0.269    ns/op
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
 				.include(".*" + SystemTimeBenchmark.class.getSimpleName() + ".*")
