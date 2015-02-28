b6e16d7a6951eab165bf04f559061ed3e293c11c
Ivan Sopov <sopov.ivan@gmail.com>
2015-02-28 17:38:18 +0300

    Teads rewrite with performance improvement

--- src/main/java/com/sopovs/moradanen/jmh/Teads.java
+++ src/main/java/com/sopovs/moradanen/jmh/Teads.java
@@ -1,12 +1,12 @@
 package com.sopovs.moradanen.jmh;
 
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Teads {
     public static void main(String args[]) {
@@ -14,116 +14,56 @@ public class Teads {
     }
 
     static int distanceFromCenter(Scanner in) {
-        Map<Integer, Node> nodes = getNodes(in);
-        ByteBuffer d = getDistances(nodes);
-        int size = nodes.size();
-        nodes = null;
-
-        return distanceFromCenter(d, size);
-    }
-
-    private static int distanceFromCenter(ByteBuffer d, int size) {
-        floydWarshall(d, size);
-
-        byte min = Byte.MAX_VALUE;
-        byte[] tmp = new byte[size];
-        d.reset();
-        for (int i = 0; i < size; i++) {
-            d.get(tmp);
-            byte max = max(tmp);
-            if (max < min) {
-                min = max;
+        Set<Node> nodes = getNodesWithoutRoots(in);
+        CenterFinder finder = new CenterFinder();
+        for (Node node : nodes) {
+            int dist = node.findMaxDistance(null, 0, finder);
+            if (dist < finder.currentMin) {
+                finder.currentMin = dist;
             }
         }
-
-        return min;
+        return finder.currentMin;
     }
 
-    private static void floydWarshall(ByteBuffer d, int size) {
-        for (int k = 0; k < size; k++) {
-            floydWarshallIteration(d, size, k);
-        }
-    }
-
-    private static void floydWarshallIteration(ByteBuffer d, int size, int k) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                int was = d.get(i * size + j);
-                int next = d.get(i * size + k) + d.get(k * size + j);
-                if (next < was) {
-                    d.put(i * size + j, (byte) next);
-                }
-            }
-        }
-    }
-
-    private static ByteBuffer getDistances(Map<Integer, Node> nodes) {
-        int size = nodes.size();
-        ByteBuffer d = ByteBuffer.allocate(size * size);
-        d.mark();
-        byte[] m = new byte[size];
-        Arrays.fill(m, Byte.MAX_VALUE);
-        for (int i = 0; i < size; i++) {
-            m[i] = 0;
-            d.put(m);
-            m[i] = Byte.MAX_VALUE;
-        }
-
-        for (Node node : nodes.values()) {
-            if (node.nodes != null) {
-                for (Node other : node.nodes) {
-                    d.put(node.index * size + other.index, (byte) 1);
-                    d.put(other.index * size + node.index, (byte) 1);
-                }
-            }
-        }
-        return d;
-    }
-
-    static Map<Integer, Node> getNodes(Scanner in) {
-        int n = in.nextInt(); // the number of adjacency relations
+    static Set<Node> getNodesWithoutRoots(Scanner in) {
+        int n = in.nextInt();
         Map<Integer, Node> nodes = new HashMap<>();
+        Set<Node> roots = new HashSet<>();
         int counter = 0;
         for (int i = 0; i < n; i++) {
-            int xi = in.nextInt(); // the ID of a person which is adjacent to yi
+            int xi = in.nextInt();
             Node xnode = nodes.get(xi);
             if (xnode == null) {
                 xnode = new Node(counter);
                 nodes.put(xi, xnode);
                 counter++;
+                roots.add(xnode);
+            } else {
+                roots.remove(xnode);
             }
 
-            int yi = in.nextInt(); // the ID of a person which is adjacent to xi
+            int yi = in.nextInt();
             Node ynode = nodes.get(yi);
             if (ynode == null) {
                 ynode = new Node(counter);
                 nodes.put(yi, ynode);
                 counter++;
+                roots.add(ynode);
+            } else {
+                roots.remove(ynode);
             }
 
-            if (xnode.nodes == null) {
-                if (ynode.nodes != null) {
-                    ynode.nodes.add(xnode);
-                } else {
-                    xnode.nodes = new ArrayList<>();
-                    xnode.nodes.add(ynode);
-                }
-            } else {
-                xnode.nodes.add(ynode);
-            }
+            ynode.nodes.add(xnode);
+            xnode.nodes.add(ynode);
         }
-        return nodes;
+
+        Set<Node> nodesWithoutRoots = new HashSet<>(nodes.values());
+        nodesWithoutRoots.removeAll(roots);
+        return nodesWithoutRoots;
     }
 
-    private static byte max(byte[] a) {
-        byte result = Byte.MIN_VALUE;
-        for (int i = 0; i < a.length; i++) {
-            byte next = a[i];
-            if (next > result) {
-                result = next;
-            }
-        }
-        return result;
+    static class CenterFinder {
+        public int currentMin = Integer.MAX_VALUE;
     }
 
     static class Node {
@@ -133,8 +73,28 @@ public class Teads {
             this.index = index;
         }
 
-        public List<Node> nodes;
+        public List<Node> nodes = new ArrayList<>();
+
+        public int findMaxDistance(Node exclude, int plus, CenterFinder finder) {
+            if (plus > finder.currentMin) {
+                return Integer.MAX_VALUE;
+            }
+            if (nodes.size() == 1 && nodes.get(0) == exclude) {
+                return plus;
+            }
+            int result = Integer.MIN_VALUE;
+            for (int i = 0; i < nodes.size(); i++) {
+                Node node = nodes.get(i);
+                if (node == exclude) {
+                    continue;
+                }
+                int dist = node.findMaxDistance(this, plus + 1, finder);
+                if (dist > result) {
+                    result = dist;
+                }
+            }
+            return result;
+        }
 
     }
-
 }
--- src/test/java/com/sopovs/moradanen/jmh/TeadsBenchmarkTest.java
+++ src/test/java/com/sopovs/moradanen/jmh/TeadsBenchmarkTest.java
@@ -3,10 +3,16 @@ package com.sopovs.moradanen.jmh;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 public class TeadsBenchmarkTest {
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(2, TimeUnit.SECONDS);
 
     @Test
     public void testOne() {
@@ -25,4 +31,21 @@ public class TeadsBenchmarkTest {
         int distance = Teads.distanceFromCenter(new Scanner(TeadsBenchmark.SIX.replace('#', '\n')));
         assertEquals(7, distance);
     }
+
+    @Test
+    public void testEight() {
+        try (Scanner in = new Scanner(TeadsBenchmarkTest.class.getResourceAsStream("/teads_eight.txt"))) {
+            int distance = Teads.distanceFromCenter(in);
+            assertEquals(9, distance);
+        }
+    }
+
+    @Test
+    public void testNine() {
+        try (Scanner in = new Scanner(TeadsBenchmarkTest.class.getResourceAsStream("/teads_nine.txt"))) {
+            int distance = Teads.distanceFromCenter(in);
+            assertEquals(15, distance);
+        }
+    }
+
 }
