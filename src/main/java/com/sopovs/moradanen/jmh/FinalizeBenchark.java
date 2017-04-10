3374ba3fe6ecf8f60c31c3b45cb20e3f5fa870df
Ivan Sopov <sopov.ivan@gmail.com>
2017-04-10 17:41:33 +0300

    fix for proper guava-helper usage

--- src/main/java/com/sopovs/moradanen/jmh/FinalizeBenchark.java
+++ src/main/java/com/sopovs/moradanen/jmh/FinalizeBenchark.java
@@ -25,12 +25,10 @@ import com.google.common.base.FinalizableReferenceQueue;
 import com.google.common.collect.Sets;
 
 //Benchmark                            Mode  Cnt     Score      Error  Units
-//FinalizeBenchark.baseline            avgt   15   141.919 ±   12.554  ns/op
-//FinalizeBenchark.finalizable         avgt   15   776.125 ±   75.940  ns/op
-//FinalizeBenchark.guardedFinalizable  avgt   15   773.450 ±  109.128  ns/op
-//FinalizeBenchark.guavaFinalizable    avgt   15  5754.685 ± 5708.960  ns/op
-//FinalizeBenchark.phantomFinalizable  avgt   15   153.219 ±    7.176  ns/op
-
+//FinalizeBenchark.baseline            avgt   15    86.274 ±   11.975  ns/op
+//FinalizeBenchark.finalizable         avgt   15  1035.979 ±  989.388  ns/op
+//FinalizeBenchark.guavaFinalizable    avgt   15  1196.080 ± 1096.015  ns/op
+//FinalizeBenchark.phantomFinalizable  avgt   15   168.042 ±   19.884  ns/op
 @BenchmarkMode(Mode.AverageTime)
 @Fork(value = 3)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -60,13 +58,6 @@ public class FinalizeBenchark {
 	}
 
 	@Benchmark
-	public void guardedFinalizable(Blackhole bh) {
-		try (GuardedFinalizable obj = new GuardedFinalizable()) {
-			bh.consume(obj);
-		}
-	}
-
-	@Benchmark
 	public void guavaFinalizable(Blackhole bh) {
 		try (GuavaFinalizable obj = new GuavaFinalizable()) {
 			bh.consume(obj);
@@ -82,23 +73,27 @@ public class FinalizeBenchark {
 
 	public static final class PhantomFinalizable implements AutoCloseable {
 		private static final ReferenceQueue<PhantomFinalizable> RFQ = new ReferenceQueue<>();
+		private static final Set<ResourcePhantomReference> REFERENCES = ConcurrentHashMap.newKeySet();
 
 		private static final Set<Object> SET = ConcurrentHashMap.newKeySet();
 		private final Object resource = new Object();
+		private final ResourcePhantomReference reference;
 
 		public PhantomFinalizable() {
 			SET.add(resource);
-			// TODO should I store this reference somewhere?
-			new ResourcePhantomReference(this, RFQ, resource);
+			reference = new ResourcePhantomReference(this, RFQ, resource);
+			REFERENCES.add(reference);
 			ResourcePhantomReference dead = (ResourcePhantomReference) RFQ.poll();
 			while (dead != null) {
 				SET.remove(dead.resource);
+				REFERENCES.remove(reference);
 				dead = (ResourcePhantomReference) RFQ.poll();
 			}
 		}
 
 		@Override
 		public void close() {
+			REFERENCES.remove(reference);
 			SET.remove(resource);
 		}
 
@@ -123,12 +118,7 @@ public class FinalizeBenchark {
 
 		public GuavaFinalizable() {
 			SET.add(resource);
-			Reference<?> reference = new FinalizablePhantomReference<GuavaFinalizable>(this, FRQ) {
-				public void finalizeReferent() {
-					REFERENCES.remove(this);
-					SET.remove(resource);
-				}
-			};
+			Reference<?> reference = new ResoureFinalizablePhantomReference(this, FRQ, resource);
 			REFERENCES.add(reference);
 		}
 
@@ -137,29 +127,22 @@ public class FinalizeBenchark {
 			SET.remove(resource);
 		}
 
-	}
+		private static final class ResoureFinalizablePhantomReference
+				extends FinalizablePhantomReference<GuavaFinalizable> {
+			private final Object resource;
 
-	public static final class GuardedFinalizable implements AutoCloseable {
-		private static final Set<Object> SET = ConcurrentHashMap.newKeySet();
-		private final Object resource = new Object();
-		private volatile boolean closed = false;
+			private ResoureFinalizablePhantomReference(GuavaFinalizable referent, FinalizableReferenceQueue queue,
+					Object resource) {
+				super(referent, queue);
+				this.resource = resource;
+			}
 
-		public GuardedFinalizable() {
-			SET.add(resource);
-		}
-
-		@Override
-		public void close() {
-			SET.remove(resource);
-			closed = true;
-		}
-
-		@Override
-		protected void finalize() {
-			if (!closed) {
+			public void finalizeReferent() {
+				REFERENCES.remove(this);
 				SET.remove(resource);
 			}
 		}
+
 	}
 
 	public static final class Finalizable implements AutoCloseable {
